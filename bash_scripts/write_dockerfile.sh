94e77740e6364387b203b2d24bd76fd925acce9b
milunadev <misusuyatec@gmail.com>
2024-04-20 18:34:38 -0500

    ADD cp .env

--- bash_scripts/write_dockerfile.sh
+++ bash_scripts/write_dockerfile.sh
@@ -22,3 +22,4 @@ EOF
 
 
 cat ../tengen-tetris/Dockerfile
+
--- tengen-tetris/application.py
+++ tengen-tetris/application.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 #Cargar variables de ambiente para el puerto e IP desde ../.env
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # Initialize the flask app.
